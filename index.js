0b182435814e5091acc7b9e1df95046a653bd2f1
Rodney Hovley <Rodney Hovley>
2016-05-13 01:23:07 -0600

    updated parse version

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://heroku_75bv4vf5:fibvt982dpbagrl1bsrrpq4b0n@ds051640.mlab.com:51640/heroku_75bv4vf5',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'test_id',
-  masterKey: process.env.MASTER_KEY || 'test_key' //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'test_key', //Add your master key here. Keep it secret!
   serverURL: "https://pencil-plop-parse.herokuapp.com/parse"
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
