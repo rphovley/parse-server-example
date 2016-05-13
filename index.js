9bb8f9b6cbccc8552a36640bca318ff71411c23c
Rodney Hovley <Rodney Hovley>
2016-05-13 01:10:23 -0600

    updated parse version

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_75bv4vf5:fibvt982dpbagrl1bsrrpq4b0n@ds051640.mlab.com:51640/heroku_75bv4vf5',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'test_id',
   masterKey: process.env.MASTER_KEY || 'test_key' //Add your master key here. Keep it secret!
