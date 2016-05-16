e3c716fc22c905c0b1f6f72860c722c82bf3e2f6
Rodney Hovley <Rodney Hovley>
2016-05-16 12:43:26 -0600

    another small change

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,4 @@
-Parse.initialize('test_ud');
-Parse.serverURL = 'https://officedev.paulhovley.com/parse';
+
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hello from Azure.');
 });
